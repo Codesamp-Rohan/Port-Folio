0936bf852a0d7b6d0a5e4c1efc5fdc968ed9f324
Codesamp-Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2023-07-14 21:38:12 +0530

    NEWW

--- script.js
+++ script.js
@@ -1,74 +1,40 @@
-window.addEventListener('scroll', reveal);
-
-function reveal() {
-    var reveals = document.querySelectorAll('.reveal');
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 200;
-
-        if (revealtop < windowheight - revealpoint) {
-            reveals[i].classList.add('active');
-        } else {
-            reveals[i].classList.remove('active');
-        }
-    }
+function resume() {
+    document.getElementById("aboutContent").style.display = "none";
+    document.getElementById("resumeContent").style.display = "block";
+    document.getElementById("portfolioContent").style.display = "none";
+    document.getElementById("contactContent").style.display = "none";
+    document.getElementById("resumeTag").style.color = "#e3c466";
+    document.getElementById("aboutTag").style.color = "#d6d6d6";
+    document.getElementById("contactTag").style.color = "#d6d6d6";
+    document.getElementById("portfolioTag").style.color = "#d6d6d6";
 }
-
-
-window.addEventListener('scroll', slideLeft);
-
-function slideLeft() {
-    var reveals = document.querySelectorAll('.slideLeft');
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 200;
-
-        if (revealtop < windowheight - revealpoint) {
-            reveals[i].classList.add('active');
-        } else {
-            reveals[i].classList.remove('active');
-        }
-    }
+function portfolio() {
+    document.getElementById("aboutContent").style.display = "none";
+    document.getElementById("resumeContent").style.display = "none";
+    document.getElementById("portfolioContent").style.display = "block";
+    document.getElementById("contactContent").style.display = "none";
+    document.getElementById("portfolioTag").style.color = "#e3c466";
+    document.getElementById("resumeTag").style.color = "#d6d6d6";
+    document.getElementById("contactTag").style.color = "#d6d6d6";
+    document.getElementById("aboutTag").style.color = "#d6d6d6";
 }
-
-
-window.addEventListener('scroll', slideRight);
-
-function slideRight() {
-    var reveals = document.querySelectorAll('.slideRight');
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 200;
-
-        if (revealtop < windowheight - revealpoint) {
-            reveals[i].classList.add('active');
-        } else {
-            reveals[i].classList.remove('active');
-        }
-    }
+function contact() {
+    document.getElementById("aboutContent").style.display = "none";
+    document.getElementById("resumeContent").style.display = "none";
+    document.getElementById("portfolioContent").style.display = "none";
+    document.getElementById("contactContent").style.display = "block";
+    document.getElementById("contactTag").style.color = "#e3c466";
+    document.getElementById("resumeTag").style.color = "#d6d6d6";
+    document.getElementById("portfolioTag").style.color = "#d6d6d6";
+    document.getElementById("aboutTag").style.color = "#d6d6d6";
 }
-
-
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
+function about() {
+    document.getElementById("aboutContent").style.display = "block";
+    document.getElementById("resumeContent").style.display = "none";
+    document.getElementById("portfolioContent").style.display = "none";
+    document.getElementById("contactContent").style.display = "none";
+    document.getElementById("aboutTag").style.color = "#e3c466";
+    document.getElementById("resumeTag").style.color = "#d6d6d6";
+    document.getElementById("contactTag").style.color = "#d6d6d6";
+    document.getElementById("portfolioTag").style.color = "#d6d6d6";
 }
