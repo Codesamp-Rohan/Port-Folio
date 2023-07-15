51f583fda9a9cb0d83c275440b49228830b605a9
Codesamp-Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2023-07-15 06:55:36 +0530

    YES

--- script.js
+++ script.js
@@ -38,3 +38,22 @@ function about() {
     document.getElementById("contactTag").style.color = "#d6d6d6";
     document.getElementById("portfolioTag").style.color = "#d6d6d6";
 }
+
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 200;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
